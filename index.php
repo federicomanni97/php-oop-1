3eb3d983b92ad7077dd3e92af22ee7c772d9bcd1
federicomanni97 <manni_federico@libero.it>
2023-12-04 14:40:59 +0100

    init scaffolding

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    include __DIR__ . '/Views/header.php';
+    include __DIR__ . '/Views/card.php';
+?>
+<section class="container">
+
+</section>
+<?php
+    include __DIR__ . '/Views/footer.php';
+?>  
