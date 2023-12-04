0d513d80fb15c47b155ca117402d1988a00a2496
federicomanni97 <manni_federico@libero.it>
2023-12-04 14:54:12 +0100

    Movie class added

---
+++ Model/movie.php
@@ -0,0 +1,11 @@
+<?php
+class Movie
+{
+    public $id;
+    public $original_title;
+    public $overview;
+    public $vote_average;
+    public $poster_path; 
+    public $original_language;
+}
+?>
