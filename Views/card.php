897c978802190d17f1b174ac331250e930cfcc2f
federicomanni97 <manni_federico@libero.it>
2023-12-04 14:44:11 +0100

    card-setup added

---
+++ Views/card.php
@@ -0,0 +1,15 @@
+<div class="col-12 col-md-4 col-lg-3">
+    <div class="card">
+        <img src="" class="card-img-top my-ratio" alt="">
+        <div class="card-body">
+            <h5 class="card-title">
+            </h5>
+            <p class="card-text">
+            </p>
+            <div class="d-flex justify-content-between align-items-flex-start">
+                <div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
