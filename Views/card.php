48a9875893d25b071ef48d87740c06cf4aee3b77
federicomanni97 <manni_federico@libero.it>
2023-12-04 15:27:48 +0100

    dynamic print on cards

--- Views/card.php
+++ Views/card.php
@@ -1,12 +1,15 @@
 <div class="col-12 col-md-4 col-lg-3">
     <div class="card">
-        <img src="" class="card-img-top my-ratio" alt="">
+        <img src="<?= $postermovie ?>" class="card-img-top my-ratio" alt="<?= $title ?>">
         <div class="card-body">
             <h5 class="card-title">
+                <?= $title ?>
             </h5>
             <p class="card-text">
+                <?= $overview ?>
             </p>
             <div class="d-flex justify-content-between align-items-flex-start">
+                <?= $vote ?>
                 <div>
                 </div>
             </div>
